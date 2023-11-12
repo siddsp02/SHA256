9d6bd97400fd8caa63b6f32a0c1a7db271340798
Sidd <sidd.s.pai@gmail.com>
2023-11-12 16:55:52 -0700

    Cleaned up code.

--- pysha256/sha256.py
+++ pysha256/sha256.py
@@ -13,8 +13,6 @@ References:
 import struct
 import sys
 
-UINT32_MAX = (1 << 32) - 1
-
 K = [
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
@@ -25,7 +23,6 @@ K = [
     0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
 ]
-
 
 def rotr32(a: int, n: int) -> int:
     return ((a >> n) | a << (32 - n))
@@ -55,8 +52,8 @@ def ls1(x: int) -> int:
     return rotr32(x, 17) ^ rotr32(x, 19) ^ (x >> 10)
 
 
-def addmu32(x: int, y: int) -> int:
-    return (x + y) & UINT32_MAX
+def u32_add(x: int, y: int) -> int:
+    return (x + y) % 2**32
 
 
 def pad_bytes(msg: bytearray) -> None:
@@ -69,10 +66,9 @@ def pad_bytes(msg: bytearray) -> None:
 
 
 def get_blocks(msg: bytes) -> list[int]:
-    w = [0] * 64
-    w[:16] = (w for w, in struct.iter_unpack(">L", msg))
+    w = list(struct.unpack(">16L", msg)) + [0]*48
     for i in range(16, 64):
-        w[i] = (ls1(w[i - 2]) + w[i - 7] + ls0(w[i - 15]) + w[i - 16]) & UINT32_MAX
+        w[i] = (ls1(w[i - 2]) + w[i - 7] + ls0(w[i - 15]) + w[i - 16]) % 2**32
     return w
 
 
@@ -91,11 +87,11 @@ def sha256(msg: bytearray) -> list[int]:
         a, b, c, d, e, f, g, h = H
         w = get_blocks(msg[t:t+64])
         for i in range(64):
-            t1 = (h + bs1(e) + ch(e, f, g) + K[i] + w[i]) & UINT32_MAX
-            t2 = (bs0(a) + maj(a, b, c)) & UINT32_MAX
-            h, g, f, e = g, f, e, (d + t1) & UINT32_MAX
-            d, c, b, a = c, b, a, (t1 + t2) & UINT32_MAX
-        H[:] = map(addmu32, H, [a, b, c, d, e, f, g, h])
+            t1 = (h + bs1(e) + ch(e, f, g) + K[i] + w[i]) % 2**32
+            t2 = (bs0(a) + maj(a, b, c)) % 2**32
+            h, g, f, e = g, f, e, (d + t1) % 2**32
+            d, c, b, a = c, b, a, (t1 + t2) % 2**32
+        H[:] = map(u32_add, H, [a, b, c, d, e, f, g, h])
     return H
 
 
