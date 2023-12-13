47b222987797a848bfc7c40a552cc9f569ebf659
Sidd <sidd.s.pai@gmail.com>
2023-12-13 16:49:26 -0700

    Updated main file.

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(int argc, char *argv[]) {
     array(char) buf = arr_new(char);
     for (char *c = argv[1]; *c; ++c)
         arr_push(buf, *c);
-    char *hash = sha256(&buf);
+    char *hash = sha256(buf);
     print_u256(hash);
     arr_free(buf);
     return 0;
