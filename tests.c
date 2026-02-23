0aa78345e1c70aea6609b4d292339d2419f1941a
Sidd <sidd.s.pai@gmail.com>
2026-02-22 17:57:12 -0700

    Added output printing for tests.

--- tests.c
+++ tests.c
@@ -1,5 +1,11 @@
 #include "sha256.h"
 
+#define PRINT_OUTPUT(buf) do {      \
+    for (size_t i = 0; i < 8; ++i)  \
+        printf("%#8lx ", buf[i]);   \
+    printf("\n");                   \
+} while (0)
+
 const uint32_t outputs[3][8] = {
     {0xba7816bf, 0x8f01cfea, 0x414140de, 0x5dae2223, 0xb00361a3, 0x96177a9c, 0xb410ff61, 0xf20015ad},
     {0x248d6a61, 0xd20638b8, 0xe5c02693, 0x0c3e6039, 0xa33ce459, 0x64ff2167, 0xf6ecedd4, 0x19db06c1},
@@ -17,15 +23,19 @@ int main() {
         0x6d, 0x6e, 0x6f, 0x70, 0x6e, 0x6f, 0x70, 0x71,
     };
     char m2[1000] = { [0 ... 999] = 0x61};
-    uint32_t out[32] = { 0 };
-
+    uint32_t out[8] = { 0 };
+    
     sha256_t s = sha256_init(m1, 3);
+    
     sha256_digest(&s, (char *) out);
+    PRINT_OUTPUT(out);
     assert(memcmp(out, outputs[0], 32) == 0);
+    
     sha256_update(&s, m1 + 3, 53);
-
     memset(out, 0, sizeof(out));
+    
     sha256_digest(&s, (char *) out);
+    PRINT_OUTPUT(out);
     assert(memcmp(out, outputs[1], 32) == 0);
 
     s = sha256_init(m2, 1000);
@@ -33,6 +43,7 @@ int main() {
         sha256_update(&s, m2, 1000);
 
     sha256_digest(&s, (char *) out);
+    PRINT_OUTPUT(out);
     assert(memcmp(out, outputs[2], 32) == 0);
     return 0;
 }
